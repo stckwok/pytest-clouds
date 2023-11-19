8a37c1bea62c0115499b45c8e9a6296f355a479c
Samuel Kwok <stckwok@hotmail.com>
2023-11-18 21:18:14 -0800

    make format fix

--- namecheck.py
+++ namecheck.py
@@ -1,23 +1,26 @@
 import click
 
+
 def logic(name):
     if name == "Marco":
         return "Polo"
     else:
         return "No Match"
 
+
 @click.command()
 @click.option("--name")
 def marco(name):
     """This is a Marco Polo function"""
-    
+
     result = logic(name)
-    click.echo(click.style('Checking Marco Polo', fg='green'))
+    click.echo(click.style("Checking Marco Polo", fg="green"))
     if result == "Polo":
-        click.echo(click.style(f"{result}",bg='blue', fg='red' ))
+        click.echo(click.style(f"{result}", bg="blue", fg="red"))
     else:
-        click.echo(click.style(f"ATTENTION! Wrong Name: {result}", 
-            blink=True, bold=True))
+        click.echo(
+            click.style(f"ATTENTION! Wrong Name: {result}", blink=True, bold=True)
+        )
 
 
 if __name__ == "__main__":
--- test_namecheck.py
+++ test_namecheck.py
@@ -2,20 +2,21 @@ from namecheck import logic
 from click.testing import CliRunner
 from namecheck import marco
 
+
 def test_marco_polo():
-  runner = CliRunner()
-  result = runner.invoke(marco, ['--name', 'Marco'])
-  assert result.exit_code == 0
-  assert 'Polo' in result.output
-  
+    runner = CliRunner()
+    result = runner.invoke(marco, ["--name", "Marco"])
+    assert result.exit_code == 0
+    assert "Polo" in result.output
+
+
 def test_marco_bob():
-  runner = CliRunner()
-  result = runner.invoke(marco, ['--name', 'Bob'])
-  assert result.exit_code == 0
-  assert 'No Match' in result.output
+    runner = CliRunner()
+    result = runner.invoke(marco, ["--name", "Bob"])
+    assert result.exit_code == 0
+    assert "No Match" in result.output
+
 
 def test_logic():
     assert "Polo" in logic("Marco")
     assert "No Match" == logic("Bob")
-    
-    
